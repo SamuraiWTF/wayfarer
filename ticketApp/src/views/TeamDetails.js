66e31b4af11a992ec638ee0377931b1d02acaa73
mgillam <mic@secureideas.com>
2021-05-13 15:08:31 -0400

    Switched team details from mock data to live data

--- ticketApp/src/views/TeamDetails.js
+++ ticketApp/src/views/TeamDetails.js
@@ -1,46 +1,32 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from "react-query";
+import AuthContext from "../components/context/AuthContext";
+import { useContext, useState } from "react";
 import TeamLabel from '../components/shared/TeamLabel';
 import TicketStats from '../components/teams/TicketStats';
 import TeamMemberList from '../components/teams/TeamMemberList';
 
 const TeamDetails = () => {
+    // Auth context 
+    const { token, clearAuth } = useContext(AuthContext);
+    
     // Hooks
     let { teamId } = useParams();
 
     // Data Fetching
 
-    const { isLoading, error, data } = useQuery(['teamTicketStats', teamId], 
-    () => { /* fetch */}, 
+    const { isLoading, error, data } = useQuery(['teamDetails', teamId], 
+    () => fetch(`http://localhost:3001/team/${teamId}`, { headers: { 'Authorization': `Bearer ${token}`}}).then(res => res.json()), 
     { /* options */ })
-    const mockData = {
-        id: 1,
-        name: "Help Desk",
-        role: "owner", // ["none", "member", "manager", "owner", "admin"] -- used for setting UI
-        members: [
-            { 
-                user_id: 3,
-                name: "John the Cook",
-                role: "member",
-                open_tickets: 2
-            },
-            {
-                user_id: 2,
-                name: "Quartermaster Sam",
-                role: "owner",
-                open_tickets: 0
-            }
-        ],
-        stats: {
-            open: 2,
-            closed: 5,
-            unassigned: 3,
-            overdue: 0
-        }
-    }
     
     // Early Renders
+    if(isLoading) {
+        return <p>Loadings...</p>
+    }
 
+    if(error) {
+        return <p>Error: {error}</p>
+    }
 
     // Event Handlers
 
@@ -61,12 +47,12 @@ const TeamDetails = () => {
                     <div className="column">
                         <h2 className="subtitle">Tickets</h2>
                         <div className="content">
-                        <TicketStats data={mockData.stats} onClick={handleStatsClick} />
+                        <TicketStats data={data.data.stats} onClick={handleStatsClick} />
                         </div>
                     </div>
                     <div className="column">
                         <h2 className="subtitle">Members</h2>
-                        <TeamMemberList data={mockData.members} onAction={handleMemberAction} />
+                        <TeamMemberList data={data.data.members} onAction={handleMemberAction} />
                     </div>
                 </div>
             </div>
