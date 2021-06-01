31d537c9c2dc63acfa7a7045ecc435771a615727
rhelgason <ryan@secureideas.com>
2021-06-01 15:02:55 -0400

    env default

--- ticketApi/src/data/mysqlConnectionPool.js
+++ ticketApi/src/data/mysqlConnectionPool.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 const pool = mysql.createPool({
-    host: 'sqldb',
+    host: process.env.DB_HOST ? process.env.DB_HOST : 'sqldb',
     user: 'ticketapp',
     password: 't1ck3tw1ck3t',
     database: 'tickets'
