2e154f2a4611424a9da6517ed2a9f2a114bd0366
root <root@a-1750rw1nmp2f1.corp.amazonworkspaces.com>
2022-07-08 16:04:47 -0400

    cherrypick login from ctq_2 brach

--- ticketApp/src/views/Login.js
+++ ticketApp/src/views/Login.js
@@ -1,6 +1,5 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import useApiOrigin from "../hooks/useApiOrigin";
-import useOAuthOrigin from "../hooks/useOAuthOrigin";
 import AuthContext from "../components/context/AuthContext";
 import { Redirect } from "react-router-dom";
 import useQueryParams from "../hooks/useQueryParams";
@@ -8,14 +7,10 @@ import useQueryParams from "../hooks/useQueryParams";
 const Login = ({ hasAuth }) => {
     const { statusChanged } = useContext(AuthContext);
     const apiOrigin = useApiOrigin();
-    const oauthOrigin = useOAuthOrigin();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [stayLoggedIn, setStayLoggedIn] = useState(false);
-    const [consenting, setConsenting] = useState(false);
     const goto = useQueryParams().get('goto');
-
-    const clientId = 'wayfarer';
 
     if(hasAuth) {
         return <Redirect to={goto || '/'} />
@@ -39,7 +34,8 @@ const Login = ({ hasAuth }) => {
                     sessionStorage.removeItem('isAdmin');
                 }
                 localStorage.setItem('currentUserId', res.userId);
-                setConsenting(true);
+                localStorage.setItem('authToken', res.token);
+                statusChanged();
             }
         })
     }
@@ -66,50 +62,10 @@ const Login = ({ hasAuth }) => {
                     localStorage.setItem('loggedInSince', Date.now());
                 }
                 localStorage.setItem('currentUserId', res.data.userId);
-                getAuthCode();
-            }
-        })
-    }
-
-    const getAuthCode = () => {
-        const userId = localStorage.getItem('currentUserId');
-        fetch(`${oauthOrigin}/authenticate?userId=${userId}&clientId=${clientId}`).then(res => res.json()).then(res => {
-            if (res.error) {
-                alert(res.error);
-            } else {
-                // need to consent if never done before or expired grant
-                if (!res.data) console.log("never before")
-                if (res.expiresAt < Math.round(Date.now() / 1000)) console.log("expired")
-                if (!res.data || res.expiresAt < Math.round(Date.now() / 1000)) setConsenting(true);
-                else getToken(res.data);
-            }
-        });
-    }
-
-    const newAuthCode = () => {
-        const userId = localStorage.getItem('currentUserId');
-        fetch(`${oauthOrigin}/authenticate/new?userId=${userId}&clientId=${clientId}`).then(res => res.json()).then(res => {
-            if (res.error) {
-                setConsenting(false);
-                alert(res.error);
-            } else {
-                getToken(res.data);
-            }
-        });
-    }
-
-    const getToken = (code) => {
-        const userId = localStorage.getItem('currentUserId');
-        const isAdmin = sessionStorage.getItem('isAdmin') ? sessionStorage.getItem('isAdmin') : false;
-        fetch(`${oauthOrigin}/token?userId=${userId}&username=${username}&code=${code}&clientId=${clientId}&isAdmin=${isAdmin}`).then(res => res.json()).then(res => {
-            if (res.error) {
-                setConsenting(false);
-                alert(res.error);
-            } else {
-                localStorage.setItem('authToken', res.data);
+                localStorage.setItem('authToken', res.data.token);
                 statusChanged();
             }
-        });
+        })
     }
 
     return (<div className="modal is-active">
@@ -127,7 +83,7 @@ const Login = ({ hasAuth }) => {
                 <div className="field-body">
                     <div className="field">
                         <p className="control">
-                            <input className="input is-large" type="username" placeholder="user@wayfarertf.test" value={username} onChange={event => setUsername(event.target.value)} disabled={consenting} />
+                            <input className="input is-large" type="username" placeholder="user@wayfarertf.test" value={username} onChange={event => setUsername(event.target.value)} />
                         </p>
                     </div>
                 </div>
@@ -139,14 +95,14 @@ const Login = ({ hasAuth }) => {
                 <div className="field-body">
                     <div className="field">
                         <p className="control">
-                            <input className="input is-large" type="password" placeholder="***********" value={password} onChange={event => setPassword(event.target.value)} disabled={consenting} />
+                            <input className="input is-large" type="password" placeholder="***********" value={password} onChange={event => setPassword(event.target.value)} />
                         </p>
                     </div>
                 </div>
             </div>
             <div className="field is-horizontal">
                 <label className="checkbox has-text-primary-light">
-                    <input type="checkbox" checked={stayLoggedIn} onChange={() => setStayLoggedIn(!stayLoggedIn)} disabled={consenting} />
+                    <input type="checkbox" checked={stayLoggedIn} onChange={() => setStayLoggedIn(!stayLoggedIn)} />
                     Stay Logged In
                 </label>
             </div>
@@ -155,14 +111,7 @@ const Login = ({ hasAuth }) => {
                 { stayLoggedIn ? `Don't use this option on shared devices.` : ''}
                 </label>
             </div>
-            <div className="field is-horizontal">
-                <label className={"has-text-primary-light is-small" + (consenting ? "" : " is-hidden")}>
-                    By confirming below, you are consenting Wayfarer to access your account.
-                </label>
-            </div>
-            <button className={"button is-link" + (consenting ? " is-hidden" : "")} onClick={handleLogin}>Login</button>
-            <button className={"button is-link mr-4" + (consenting ? "" : " is-hidden")} onClick={newAuthCode}>Confirm</button>
-            <button className={"button is-link ml-4" + (consenting ? "" : " is-hidden")} onClick={() => setConsenting(false)}>Cancel</button>
+            <button className="button is-link" onClick={handleLogin}>Login</button>
         </div>
     </div>
     )
